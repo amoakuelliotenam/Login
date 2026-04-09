a3f5b7b3e54fcd517e43cc86f5ef9cb4cb938021
amoakuelliotenam <amoakuelliotenam@gmail.com>
2026-04-09 19:18:01 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+async function register() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+  alert(data.message);
+}
+
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const res = await fetch("/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  });
+
+  const data = await res.json();
+
+  if (data.success) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Invalid login details");
+  }
+}
